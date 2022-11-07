4342b50af04f2edfa0b6ae1ef87dc54e20b65402
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-11-06 22:00:02 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  return <div className="App">text_all_here</div>;
+}
+
+export default App;
---
+++ src/components/_template.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function _template() {
+  // * SCRIPT
+  return (
+    // * TEMPLATE
+    <div>_template</div>
+  );
+}
